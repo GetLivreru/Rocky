42507154658cd9b5b2aa933e586a724a08992346
GitLivreru <adilet.25.anar@gmail.com>
2026-04-05 23:02:28 +0500

    Implement basic ArrayList example in arraylist.java

---
+++ arraylist/arraylist.java
@@ -0,0 +1,12 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list);
+    }
+}
